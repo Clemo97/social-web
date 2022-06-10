20a10d42a80003d6ae5e2059172abe0072ef0aba
Clemo97 <lumumbaclement@gmail.com>
2022-06-11 00:34:53 +0300

    fix css error

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-from django.contrib.auth views as auth_views
 from . import views
 
 urlpatterns = [
     # post views
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('login/', views.user_login, name='login'),
 ]
